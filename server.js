5a215f5b07ea54cb522a8339137b0908d16eddee
taurtk <taurtk@gmail.com>
2020-07-22 17:20:25 +0545

    add login POST route

--- server.js
+++ server.js
@@ -28,3 +28,30 @@ app.get('/users/:id',(req,res)=>{
         user: req.params.id
     })
 } )
+
+const bodyparser = require('body-parser');
+app.use(bodyparser.json())
+
+app.post('/login', ()=>
+{
+    const username= req.body.username;
+    const password = req.body.password;
+    const mockUsername ="billyTheKid";
+    const mockPassword ="superSecret";
+
+    if(username === mockUsername && password === mockPassword)
+    {
+        res.json({
+            success:true,
+            message: 'password and username match!',
+            token:'encrypted token goes here'
+        })
+    }
+    else
+    {
+        res.json({
+            success:false,
+            message:'password and username do not match'
+        })
+    }
+})
